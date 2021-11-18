24fa3aa8d19eca73fe42ca9970552670a97abe4d
josef_cit <josef@ciandt.com>
2021-11-18 15:33:01 -0300

    Desafio concluido

---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+
+import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
+
+class ListAllUsersController {
+  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+
+  handle(request: Request, response: Response): Response {
+    const user_id = request.headers.user_id.toString();
+    try {
+      const users = this.listAllUsersUseCase.execute({ user_id });
+
+      return response.json(users);
+    } catch (e) {
+      return response.status(400).json({ error: e });
+    }
+    return response.send();
+  }
+}
+
+export { ListAllUsersController };
